760988b8eeaddff321687b537c42cd1a109cded9
Srijan Nayak <srijannayak0@gmail.com>
2019-05-17 20:02:23 +0530

    Add print_pause function

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("Hello world")
+import time
+
+
+def print_pause(string: str, seconds: int):
+    """Print a passed in string after a
+    delay of passed in seconds.
+    """
+    time.sleep(seconds)
+    print(string)
