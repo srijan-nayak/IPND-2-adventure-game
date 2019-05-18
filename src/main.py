5a1c0b3c12a037165542b1d90f50d3cddcbdd301
Srijan Nayak <srijannayak0@gmail.com>
2019-05-18 17:10:25 +0530

    Add return type annotation to print_pause

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import time
 
 
-def print_pause(string: str, delay=2):
+def print_pause(string: str, delay=2)->None:
     """Print a passed in string after a
     delay of passed in seconds. Default
     delay is of 2 seconds.
