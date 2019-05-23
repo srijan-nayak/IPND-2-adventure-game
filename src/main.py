f683567757a4a0ebc62f79c58a745792cfc42fbd
Srijan Nayak <srijannayak0@gmail.com>
2019-05-23 12:38:05 +0530

    Add intro part for the game

--- src/main.py
+++ src/main.py
@@ -12,3 +12,27 @@ def print_pause(string: str, delay=2):
 
 enemies = ["Red Dragon", "Armoured Ogre", "Ferocious Minotaur"]
 enemy = random.choice(enemies)
+
+while True:
+    print_pause("You are in an empty corridor deep in a dungeon.")
+    print_pause(f"You have come here to kill the {enemy}.")
+    print_pause("The problem is that you broke you trusty sword "
+                "while making your way till here.")
+    print_pause("What do you do?")
+    while True:
+        print_pause("1. Give up and go back home", 1)
+        print_pause("2. Continue this hopeless quest", 1)
+        print_pause("Enter one of the option numbers.")
+        choice = input()
+        if choice == "1":
+            print_pause("You exit the dungeon and go back home.")
+            print_pause("GAME OVER!")
+            exit()
+        elif choice == 2:
+            break
+        else:
+            print_pause("Invalid input! Enter again.")
+    print_pause("Would you like to try again?")
+    choice = input().lower()
+    if choice != "y":
+        break
