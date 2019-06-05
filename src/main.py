d95571d7b23a06295c6ebde377f7e9b73941ac43
Srijan Nayak <srijannayak0@gmail.com>
2019-06-05 10:38:55 +0530

    Move black door code inside a separate function

--- src/main.py
+++ src/main.py
@@ -40,6 +40,95 @@ def enter_blue_door(items: list)->None:
     print_pause("You leave the room.")
 
 
+def enter_black_door(items: list)->bool:
+    print_pause("You take a deep breath before you open the "
+                "black door.")
+    print_pause("You know what's beyond this door.")
+    print_pause("You open the door.")
+    print_pause(f"You find the {enemy} in the room.")
+    while True:
+        print_pause("What do you do?")
+        option_count = 0
+        if writing_utensil in items:
+            option_count += 1
+            print_pause(f"{option_count}. Fight him with your pen", 1)
+        if "deathnote" in items:
+            option_count += 1
+            print_pause(f"{option_count}. Ask for his autograph", 1)
+        option_count += 1
+        print_pause(f"{option_count}. Run back to the corridor.", 1)
+        print_pause("Enter one of the option numbers.", 0)
+        choice = input()
+        if option_count == 3:
+            if choice == "1":
+                print_pause("You uncap your pen that you just found.")
+                print_pause(f"You rush towards the {enemy} swinging "
+                            "your pen.")
+                print_pause("But you never stood a chance with your "
+                            "pen.")
+                print_pause(f"The {enemy} renders you unfit for "
+                            "battle with a single attack.")
+                return False
+            elif choice == "2":
+                print_pause("You gather all your courage and ask "
+                            f"the {enemy} for his autograph.")
+                print_pause(f"Agreeing to your request, the {enemy} "
+                            "takes your DEATHNOTE and "
+                            f"your {writing_utensil} for signing.")
+                print_pause("While blushing. A lot.")
+                print_pause("He signs his name in the DEATHNOTE.")
+                print_pause("You take back the DEATHNOTE and the pen.")
+                print_pause(f"The {enemy} suddenly has a heart attack "
+                            "10 seconds after signing his name.")
+                print_pause(f"The {enemy} is dead!")
+                print_pause("Looks like the DEATHNOTE worked.")
+                print_pause("Congrats!")
+                print_pause(f"You have killed the {enemy}!")
+                print_pause("You won!")
+                return False
+            elif choice == "3":
+                print_pause("You run back to the corridor.")
+                print_pause(f"Looks like the {enemy} has not "
+                            "followed you.")
+                break
+            else:
+                print_pause("Invalid input! Enter again.")
+        elif option_count == 2:
+            if choice == "1" and writing_utensil in items:
+                print_pause("You uncap your pen that you just found.")
+                print_pause(f"You rush towards the {enemy} swinging "
+                            "your pen.")
+                print_pause("But you never stood a chance with your "
+                            "pen.")
+                print_pause(f"The {enemy} renders you unfit for "
+                            "battle with a single attack.")
+                return False
+            elif choice == "1" and "deathnote" in items:
+                print_pause("You gather all your courage and ask "
+                            f"the {enemy} for his autograph.")
+                print_pause(f"But the {enemy} is annoyed that you "
+                            "don't have a writing utensil!")
+                print_pause(f"The {enemy} renders you unfit for "
+                            "battle with a single attack.")
+                return False
+            elif choice == "2":
+                print_pause("You run back to the corridor.")
+                print_pause(f"Looks like the {enemy} has not "
+                            "followed you.")
+                break
+            else:
+                print_pause("Invalid input! Enter again.")
+        elif option_count == 1:
+            if choice == "1":
+                print_pause("You run back to the corridor.")
+                print_pause(f"Looks like the {enemy} has not "
+                            "followed you.")
+                break
+            else:
+                print_pause("Invalid input! Enter again.")
+    return True
+
+
 enemies = ["Red Dragon", "Armoured Ogre", "Ferocious Minotaur"]
 writing_utensils = ["pen", "pencil", "marker"]
 choice = ""
@@ -83,95 +172,7 @@ while True:
         elif choice == "2":
             enter_blue_door(items)
         elif choice == "3":
-            print_pause("You take a deep breath before you open the "
-                        "black door.")
-            print_pause("You know what's beyond this door.")
-            print_pause("You open the door.")
-            print_pause(f"You find the {enemy} in the room.")
-            while True:
-                print_pause("What do you do?")
-                option_count = 0
-                if writing_utensil in items:
-                    option_count += 1
-                    print_pause(f"{option_count}. Fight him with your pen", 1)
-                if "deathnote" in items:
-                    option_count += 1
-                    print_pause(f"{option_count}. Ask for his autograph", 1)
-                option_count += 1
-                print_pause(f"{option_count}. Run back to the corridor.", 1)
-                print_pause("Enter one of the option numbers.", 0)
-                choice = input()
-                if option_count == 3:
-                    if choice == "1":
-                        print_pause("You uncap your pen that you just found.")
-                        print_pause(f"You rush towards the {enemy} swinging "
-                                    "your pen.")
-                        print_pause("But you never stood a chance with your "
-                                    "pen.")
-                        print_pause(f"The {enemy} renders you unfit for "
-                                    "battle with a single attack.")
-                        repeatCorridorChoice = False
-                        break
-                    elif choice == "2":
-                        print_pause("You gather all your courage and ask "
-                                    f"the {enemy} for his autograph.")
-                        print_pause(f"Agreeing to your request, the {enemy} "
-                                    "takes your DEATHNOTE and "
-                                    f"your {writing_utensil} for signing.")
-                        print_pause("While blushing. A lot.")
-                        print_pause("He signs his name in the DEATHNOTE.")
-                        print_pause("You take back the DEATHNOTE and the pen.")
-                        print_pause(f"The {enemy} suddenly has a heart attack "
-                                    "10 seconds after signing his name.")
-                        print_pause(f"The {enemy} is dead!")
-                        print_pause("Looks like the DEATHNOTE worked.")
-                        print_pause("Congrats!")
-                        print_pause(f"You have killed the {enemy}!")
-                        print_pause("You won!")
-                        repeatCorridorChoice = False
-                        break
-                    elif choice == "3":
-                        print_pause("You run back to the corridor.")
-                        print_pause(f"Looks like the {enemy} has not "
-                                    "followed you.")
-                        break
-                    else:
-                        print_pause("Invalid input! Enter again.")
-                elif option_count == 2:
-                    if choice == "1" and writing_utensil in items:
-                        print_pause("You uncap your pen that you just found.")
-                        print_pause(f"You rush towards the {enemy} swinging "
-                                    "your pen.")
-                        print_pause("But you never stood a chance with your "
-                                    "pen.")
-                        print_pause(f"The {enemy} renders you unfit for "
-                                    "battle with a single attack.")
-                        repeatCorridorChoice = False
-                        break
-                    elif choice == "1" and "deathnote" in items:
-                        print_pause("You gather all your courage and ask "
-                                    f"the {enemy} for his autograph.")
-                        print_pause(f"But the {enemy} is annoyed that you "
-                                    "don't have a writing utensil!")
-                        print_pause(f"The {enemy} renders you unfit for "
-                                    "battle with a single attack.")
-                        repeatCorridorChoice = False
-                        break
-                    elif choice == "2":
-                        print_pause("You run back to the corridor.")
-                        print_pause(f"Looks like the {enemy} has not "
-                                    "followed you.")
-                        break
-                    else:
-                        print_pause("Invalid input! Enter again.")
-                elif option_count == 1:
-                    if choice == "1":
-                        print_pause("You run back to the corridor.")
-                        print_pause(f"Looks like the {enemy} has not "
-                                    "followed you.")
-                        break
-                    else:
-                        print_pause("Invalid input! Enter again.")
+            repeatCorridorChoice = enter_black_door(items)
         else:
             print_pause("Invalid input! Enter again.")
     print_pause("Would you like to try again?")
